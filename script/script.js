9b1c199fa06f65118bece930e67f748df37ae7a0
sayedhasandipto <sayedhasandipto@gmail.com>
2026-02-23 20:05:28 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,50 @@
+console.log('script.js');
+
+
+
+
+
+
+// Button Toggle
+let interviewList = [];
+let rejectedList = [];
+
+
+
+let totalJobCount = document.getElementById('total-count');
+let interviewJobCount = document.getElementById('interview-count');
+let rejectedJobCount = document.getElementById('reject-count');
+const jobContainer = document.getElementById('job-container');
+const mainContainer = document.querySelector('main');
+console.log(mainContainer);
+
+// count function
+
+function calculateJobCount() {
+    totalJobCount.innerText = jobContainer.children.length;
+    interviewJobCount.innerText = interviewList.length;
+    rejectedJobCount.innerText = rejectedList.length;
+}
+calculateJobCount();
+
+// toggle function
+
+const allFiltterButton = document.getElementById('all-filter-button');
+const interviewFiltterButton = document.getElementById('interview-filter-button');
+const rejectedFiltterButton = document.getElementById('rejecte-filter-button');
+
+function toggleButton(id) {
+    console.log(id);
+    allFiltterButton.classList.remove('btn-info', 'text-white');
+    interviewFiltterButton.classList.remove('btn-info', 'text-white');
+    rejectedFiltterButton.classList.remove('btn-info', 'text-white');
+
+    allFiltterButton.classList.add('btn-primary-content', 'text-black');
+    interviewFiltterButton.classList.add('btn-primary-content', 'text-black');
+    rejectedFiltterButton.classList.add('btn-primary-content', 'text-black');
+
+    const selectedButton = document.getElementById(id);
+    // console.log(selectedButton);
+    selectedButton.classList.remove('btn-primary-content', 'text-black');
+    selectedButton.classList.add('btn-info', 'text-white');
+}
